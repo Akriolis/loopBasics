c665d1cf372cdd969a91c710fdcd1f0af7023cf1
Akriolis <Akriolis@gmail.com>
2022-08-26 16:44:13 +0400

    Added Practice.kt file with some practice code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,10 +8,16 @@ fun main(){
     println("After the loop. x = $x.")
 
     val f = 5
-    val y = 1
+    val y = 10
     if (f > y){
         println("f is greater than y")
     }else{
-        println("This line runs no matter what")
+        println("y is greater than f")
     }
+    println("This line runs no matter what")
+    // one line version of the same code
+    // if (f > y) "x is greater than y" else "x is not greater than y"
+    // println(if (f > y) "x is greater than y" else "x is not greater than y")
+
+
 }
